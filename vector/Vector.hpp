c7a900b2ef9b10fa50e3cf199a33646f9f24068e
theuser <Roman>
2020-11-19 23:20:46 +0300

    1

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 02:38:00 by jeldora           #+#    #+#             */
-/*   Updated: 2020/11/19 03:26:10 by jeldora          ###   ########.fr       */
+/*   Updated: 2020/11/19 23:20:17 by jeldora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,38 @@
 #pragma once
 #include <memory>
 #include <cstdlib>
+#include <iostream>
 
 namespace ft
 {
 	template <typename T, typename Alloc = std::allocator<T> >
 	class vector
 	{	
+		private:
+			Alloc	alloc;
+			T		*c;
+			size_t	size;
 		public:
-			vector()
+			
+			explicit vector()// Base constructor
+			{c = NULL;}
+
+			explicit vector(size_t n, \
+							const T& val = 0) // Fill constructor [Узнать, почемy const]
 			{
-				c = NULL;
+				c = alloc.allocate(n);
+				for (size_t i = 0; i < n; i++)
+					c[i] = val;
 			}
 
-
-		private:
-			Alloc alloc;
-			T *c;
+			vector(const vector& copy)
+			{
+				alloc = copy.alloc;
+				size = copy.size;
+				c = alloc.allocate(size);
+				for (int i = 0; i < size; i++)
+					c[i] = copy.c[i];
+			}
 
 	};
 }
--- vector/main.cpp
+++ vector/main.cpp
@@ -6,20 +6,33 @@
 /*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 01:57:46 by jeldora           #+#    #+#             */
-/*   Updated: 2020/11/19 03:24:26 by jeldora          ###   ########.fr       */
+/*   Updated: 2020/11/19 23:09:02 by jeldora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Vector.hpp"
 #include <vector>
 #include <iostream>
+#include <memory>
+
+class SimpleClass
+{
+	public:
+		int a;
+		SimpleClass(int set_a)
+		{
+			a = set_a;
+		}
+};
+
 
 int		main(void)
 {
-	//std::vector<int> a;
-	//std::cout << a[0] << "\n";
+	
+	ft::vector<int> b(5, 5);
 
-	ft::vector<char> b;
+	for(int i = 0; i < 5; i++)
+		std::cout << b.c[i] << "\n";
 
 	return (0);
 }
