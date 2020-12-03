779edd442f7cd5ff763f7287eb3891662f3e516a
jeldora <jeldora@student.21-school.ru>
2020-12-03 04:06:34 +0300

    Начал делать map

---
+++ map.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 00:06:59 by jeldora           #+#    #+#             */
+/*   Updated: 2020/12/03 04:06:00 by jeldora          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <functional>
+
+namespace ft
+{
+
+	
+
+	template < class Key, class T, typename Compare = std::less<Key> >
+	class map
+	{
+
+		private:
+				
+	};
+}
