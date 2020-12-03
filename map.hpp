18b84027c67adbcb43c1370144a6703c7ee43079
Janelle Eldora <jeldora@vo-j3.msk.21-school.ru>
2020-12-03 12:56:50 +0300

    Подготавливаю итератор для мапы

--- map.hpp
+++ map.hpp
@@ -6,45 +6,56 @@
 /*   By: jeldora <jeldora@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 00:06:59 by jeldora           #+#    #+#             */
-/*   Updated: 2020/12/03 06:48:19 by jeldora          ###   ########.fr       */
+/*   Updated: 2020/12/03 12:55:52 by jeldora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <functional>
+#include <utility>
 
 namespace ft
 {
 
-	
-
 	template < class Key, class T, typename Compare = std::less<Key> >
 	class map
 	{
+		typedef std::pair<const Key, T> value_type;
 
 		private:
-			typedef struct		s_elem
+			typedef struct			s_elem
 			{
-				s_elem			*parent;
-				s_elem 			*left;
-				s_elem 			*right;
-				Key				key;
-				T				content;
-			}					t_elem;
+				s_elem				*parent;
+				s_elem 				*left;
+				s_elem 				*right;
+				value_type			content;
+			}						t_elem;
 
-			t_elem				*root;
-			size_t				length;
+			t_elem					*root;
+			size_t					length;
+			t_elem 					*newElem(	t_elem *parent = NULL, \
+												t_elem *left = NULL, \
+												t_elem *right = NULL, \
+												value_type content = pair<Key, T>(Key(), T()))
+			{
+				t_elem *new_elem = new t_elem;
+				new_elem->parent = parent;
+				new_elem->left = left;
+				new_elem->right = right;
+				new_elem->content = content;
+				return new_elem;
+			}
 		public:
 			map()
 			{
 				root = NULL;
 				length = 0;
 			}
+
 			class iterator
 			{
-				private:
-					t_elem *elem;
 				public:
+					t_elem *elem;
 					iterator()
 					{
 						elem = NULL;
@@ -60,22 +71,79 @@ namespace ft
 							elem = elem->right;
 							return (*this);
 						}
-						if (elem->parent != NULL && elem->parent->key > elem->key) // Если это левый лист
+						if (elem->parent != NULL && elem->parent->content.first > elem->content.first) // Если это левый лист
 						{
 							elem = elem->parent;
 							return (*this);
 						}
 						t_elem *tmp = elem;
-						while (tmp->parent != NULL && tmp->parent->key < tmp->key) // Если это правый лист
+						while (tmp->parent != NULL && tmp->parent->content.first < tmp->content.first) // Если это правый лист
 							tmp = elem->parent;
 						if (tmp->parent = NULL)
 							return *this;
-						else if (tmp->parent->key > tmp->key)
+						else if (tmp->parent->content.first > tmp->content.first)
 						{
 							elem = tmp->parent;
 							return *this;
 						}
 					}
+					iterator &operator--()
+					{
+						if (elem->left != NULL) // Если есть слево элемент
+						{
+							elem = elem->left;
+							return (*this);
+						}
+						if (elem->parent != NULL && elem->parent->content.first < elem->content.first) // Если это правый лист
+						{
+							elem = elem->parent;
+							return (*this);
+						}
+						t_elem *tmp = elem;
+						while (tmp->parent != NULL && tmp->parent->content.first > tmp->content.first) // Если это левый лист
+							tmp = elem->parent;
+						if (tmp->parent = NULL)
+							return *this;
+						else if (tmp->parent->content.first < tmp->content.first)
+						{
+							elem = tmp->parent;
+							return *this;
+						}
+					}
+					value_type &operator*()
+					{
+						return (elem->content);
+					}
 			};
+			iterator begin()
+			{
+				iterator it(root);
+				t_elem *prev = NULL;
+				while (it.elem != prev)
+				{
+					prev = it.elem;
+					--it;
+				}
+				return it;
+			}
+			iterator end()
+			{
+				iterator it(root);
+				t_elem *next = NULL;
+				while (it.elem != next)
+				{
+					next = it.elem;
+					++it;
+				}
+				return it;
+			}
+			/*value_type insert (const value_type& val)
+			{
+				if (root == NULL)
+				{
+					root = newElem(NULL, NULL, NULL, val);
+					value_type ret(); // добавить итератор
+				}
+			};*/
 	};
 }
